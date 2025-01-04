7f2c90cc67c13c275e36b65b376335a827b3f6b8
Travis Hayes <70296036+travage@users.noreply.github.com>
2025-01-03 23:49:37 -0700

    Continued work on search function

--- ScraperAPI/app.py
+++ ScraperAPI/app.py
@@ -1,5 +1,5 @@
 #  . .venv/bin/activate
-from flask import Flask
+from flask import Flask, request
 from bs4 import BeautifulSoup
 import requests
 
@@ -10,15 +10,28 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
 }
 
-searchTerms = 'john summit'
-
-@app.route('/search')    # http://127.0.0.1:5000/search
+# Function should eventually return JSON doc with search results
+@app.post('/search')    # http://127.0.0.1:5000/search
 def search():
-
+    # Extracts search terms from form data
+    request_data = request.form.get('search_terms')
+    # Main 1001Tracklist URL for conducting searches
     url = 'https://www.1001tracklists.com/search/result.php'
     payload = {
-        'main_search': searchTerms,
+        'main_search': request_data,
         'search_selection': '9'
     }
-    response = requests.post(url, data=payload, headers=headers)
-    return response.text
+    html_response = requests.post(url, data=payload, headers=headers).text
+    soup = BeautifulSoup(html_response, 'lxml')
+
+    search_results = {}
+    result_num = 1
+    # Grab all rows of the results page
+    search_row = soup.find_all('div', class_='bItm action oItm')
+    for row in search_row:
+        title = row.select_one('.bTitle').text.strip()
+        search_results[result_num] = title
+        result_num += 1
+
+    return search_results
+
