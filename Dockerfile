b2d95b04512dff2e760d519a6ec20451ad22e4ed
Travis Hayes <70296036+travage@users.noreply.github.com>
2025-02-22 14:17:37 -0700

    Further adjusts made to Docker file and app.py to ready it for deployment

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir --upgrade -r requirements.txt
 COPY . .
 ENV FLASK_APP=app.py
-CMD ["gunicorn", "--bind", "0.0.0.0:80", "app:create_app()"]
+CMD ["gunicorn", "--bind", "0.0.0.0:8080", "app:create_app()"]
 
--- app.py
+++ app.py
@@ -2,80 +2,89 @@
 from flask import Flask, request
 from bs4 import BeautifulSoup
 import requests
+import os
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
 # Adjust headers to avoid bot detection
-headers = {
+    headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
 }
 
-@app.post('/search')    # http://127.0.0.1:5000/search
-def search():
-    # Extracts search terms from form data
-    request_data = request.form.get('search_terms')
-    # Main 1001Tracklist URL for conducting searches
-    url = 'https://www.1001tracklists.com/search/result.php'
-    payload = {
-        'main_search': request_data,
-        'search_selection': '9'
-    }
-    html_response = requests.post(url, data=payload, headers=headers).text
-    soup = BeautifulSoup(html_response, 'lxml')
+    @app.post('/search')    # http://127.0.0.1:5000/search
+    def search():
+        # Extracts search terms from form data
+        request_data = request.form.get('search_terms')
+        # Main 1001Tracklist URL for conducting searches
+        url = 'https://www.1001tracklists.com/search/result.php'
+        payload = {
+            'main_search': request_data,
+            'search_selection': '9'
+        }
+        html_response = requests.post(url, data=payload, headers=headers).text
+        soup = BeautifulSoup(html_response, 'lxml')
 
-    search_results = {}
-    result_num = 1
-    # Grab all rows of the results page
-    search_row = soup.find_all('div', class_='bItm action oItm')
-    # Fill the dict with all the results
-    for row in search_row:
-        title = row.select_one('.bTitle').text.strip()
-        link = row.find('a').get('href')
-        search_results[result_num] = [title, link]
-        result_num += 1
+        search_results = {}
+        result_num = 1
+        # Grab all rows of the results page
+        search_row = soup.find_all('div', class_='bItm action oItm')
+        # Fill the dict with all the results
+        for row in search_row:
+            title = row.select_one('.bTitle').text.strip()
+            link = row.find('a').get('href')
+            search_results[result_num] = [title, link]
+            result_num += 1
 
-    # Flask will send dicts as JSON docs
-    return search_results
+        # Flask will send dicts as JSON docs
+        return search_results
 
-@app.post('/tracklist')    # http://127.0.0.1:5000/tracklist
-def get_tracklist():
-    tracklist_url = request.form.get('url')
-    tracklist_html = requests.get(tracklist_url, headers=headers).text
-    soup = BeautifulSoup(tracklist_html, 'lxml')
+    @app.post('/tracklist')    # http://127.0.0.1:5000/tracklist
+    def get_tracklist():
+        tracklist_url = request.form.get('url')
+        tracklist_html = requests.get(tracklist_url, headers=headers).text
+        soup = BeautifulSoup(tracklist_html, 'lxml')
 
-    # Grab each row of the tracklist
-    tracklist_row = soup.find_all('div', class_='tlpTog')
-    # Keeps track of the row number
-    counter = 1
-    tracklist = {}
-    for row in tracklist_row:
-        # Some rows will not have HTML elements with the specified HTML classes,
-        # in which case bs4 sets the variable to None.
-        # Since the text is being extracted from the HTML element, an exception
-        # will occur if the variable is None.
-        try:
-            track_position = row.select_one('.fontXL').text.strip()
-        except:
-            track_position = '-'  # No track_position means the track is part of the mashup above it
+        # Grab each row of the tracklist
+        tracklist_row = soup.find_all('div', class_='tlpTog')
+        # Keeps track of the row number
+        counter = 1
+        tracklist = {}
+        for row in tracklist_row:
+            # Some rows will not have HTML elements with the specified HTML classes,
+            # in which case bs4 sets the variable to None.
+            # Since the text is being extracted from the HTML element, an exception
+            # will occur if the variable is None.
+            try:
+                track_position = row.select_one('.fontXL').text.strip()
+            except:
+                track_position = '-'  # No track_position means the track is part of the mashup above it
 
-        try:
-            timestamp = row.select_one('.cue.noWrap.action.mt5').text.strip()  # Sometimes the tag is there but has no text, so timestamp = ''
-        except:
-            timestamp = 'NA'
+            try:
+                timestamp = row.select_one('.cue.noWrap.action.mt5').text.strip()  # Sometimes the tag is there but has no text, so timestamp = ''
+            except:
+                timestamp = 'NA'
 
-        try:
-            track = row.select_one('.trackValue').text.strip()
-        except:
-            track = 'NA'
+            try:
+                track = row.select_one('.trackValue').text.strip()
+            except:
+                track = 'NA'
 
-        # Captures version (edit) of the song (e.g. acapella, intro edit, etc.)
-        try:
-            edit_data = row.select_one('.trackEditData').text.strip()
-        except:
-            edit_data = 'NA'
+            # Captures version (edit) of the song (e.g. acapella, intro edit, etc.)
+            try:
+                edit_data = row.select_one('.trackEditData').text.strip()
+            except:
+                edit_data = 'NA'
 
-        tracklist[counter] = [track_position, timestamp, track, edit_data]
-        counter += 1
+            tracklist[counter] = [track_position, timestamp, track, edit_data]
+            counter += 1
 
-    # Flask send dicts as JSON docs
-    return tracklist
+        # Flask send dicts as JSON docs
+        return tracklist
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
