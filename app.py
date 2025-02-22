dde0a3831113625676aa15de3e01b1f7d214dbda
Travis Hayes <70296036+travage@users.noreply.github.com>
2025-02-22 16:00:24 -0700

    Fixed typo in comment

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def create_app():
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
 }
 
-    @app.post('/search')    # http://127.0.0.1:5000/search used in developemtn
+    @app.post('/search')    # http://127.0.0.1:5000/search used in development
     def search():
         try:
             # Extracts search terms from form data
